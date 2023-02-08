d365d04dccc1b82f0c461923076de384f291b801
Rai0 <43114095+Rai0@users.noreply.github.com>
2023-02-08 19:26:01 +0300

    get price from binance

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-from requests import *
+from requests import get
 
-main_binance_endpoint = "https://api.binance.com"
+main_binance_endpoint = "https://api.binance.com/api/v3/"
 
+requests_price = "ticker/price"
+requests_klines = "klines"
+requests_interval = "1s"
+
+xrpusd = "XRPUSDT"
+
+price = get(f"{main_binance_endpoint}{requests_price}?symbol={xrpusd}").json()
+best_h_price = get(f"{main_binance_endpoint}{requests_klines}?symbol={xrpusd}&interval={requests_interval}").text
+
+# print(price["price"])
+print(best_h_price)
