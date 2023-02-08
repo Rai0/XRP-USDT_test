cba2e3850b7d8ad2fe2aeae7f6e952bf14b732b9
Rai0 <43114095+Rai0@users.noreply.github.com>
2023-02-08 19:39:12 +0300

    getting highest price per hour

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 from requests import get
+from pandas import DataFrame
 
 main_binance_endpoint = "https://api.binance.com/api/v3/"
 
 requests_price = "ticker/price"
 requests_klines = "klines"
-requests_interval = "1s"
+requests_interval = "1h"
 
 xrpusd = "XRPUSDT"
 
 price = get(f"{main_binance_endpoint}{requests_price}?symbol={xrpusd}").json()
-best_h_price = get(f"{main_binance_endpoint}{requests_klines}?symbol={xrpusd}&interval={requests_interval}").text
+
+best_h_price = get(f"{main_binance_endpoint}{requests_klines}?symbol={xrpusd}&interval={requests_interval}").json()
+
+# print(best_h_price)
+
+df = DataFrame(best_h_price).iloc[:, :5]
+
+df.columns = list("tohlc")
 
 # print(price["price"])
-print(best_h_price)
+print(df["h"].max())
